4d50a137061d91637986f1faf86f6d1cc0c43c50
cmayornavarro <cmayornavarro@gmail.com>
2021-01-10 21:25:20 +0100

    first project microstreaming

---
+++ micostreaming/src/main/java/com/streaming/micostreaming/MicostreamingApplication.java
@@ -0,0 +1,13 @@
+package com.streaming.micostreaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicostreamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicostreamingApplication.class, args);
+	}
+
+}
---
+++ micostreaming/src/test/java/com/streaming/micostreaming/MicostreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streaming.micostreaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicostreamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
