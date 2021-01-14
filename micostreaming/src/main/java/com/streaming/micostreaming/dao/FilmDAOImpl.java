9f7e68d57a5d90f9ece2b38c8fef5316c8520802
cmayornavarro <cmayornavarro@gmail.com>
2021-01-14 18:03:54 +0100

    adding FilmDAOImpl

---
+++ micostreaming/src/main/java/com/streaming/micostreaming/dao/FilmDAOImpl.java
@@ -0,0 +1,26 @@
+package com.streaming.micostreaming.dao;
+
+import com.streaming.micostreaming.model.Film;
+
+import java.util.List;
+
+public class FilmDAOImpl implements FilmDAO {   
+
+	@Override
+	public List<Film> findAll() {
+		
+		return null;
+	}
+
+	@Override
+	public void FilmfindById(int id) {
+		return;
+		
+	}
+
+	@Override
+	public void Filmsave(Film film) {
+		return;
+		
+	}
+}
