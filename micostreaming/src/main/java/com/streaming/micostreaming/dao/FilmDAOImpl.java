cfb1019ed3064c0be344c22522594a156407aa1a
cmayornavarro <cmayornavarro@gmail.com>
2021-01-18 09:49:14 +0100

    implementation of the Film interface

--- micostreaming/src/main/java/com/streaming/micostreaming/dao/FilmDAOImpl.java
+++ micostreaming/src/main/java/com/streaming/micostreaming/dao/FilmDAOImpl.java
@@ -2,25 +2,49 @@ package com.streaming.micostreaming.dao;
 
 import com.streaming.micostreaming.model.Film;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+/*
+ * @Repository : cette annotation est appliquée à la classe afin d'indiquer à
+ *  Spring qu'il s'agit d'une classe qui gère les données, ce qui nous
+ *   permettra de profiter de certaines fonctionnalités comme les translations
+ *    des erreurs
+ * */
+@Repository
 public class FilmDAOImpl implements FilmDAO {   
+    public static List<Film> films =new ArrayList<>();
+    
+    static  {
+    	films.add(new Film(1, new String("Ordinateur portable"), 350));
+    	films.add(new Film(2, new String("Aspirateur Robot"), 500)); 
+    	films.add(new Film(3, new String("Table de Ping Pong"), 750));
+	}
+   
 
+    
 	@Override
 	public List<Film> findAll() {
 		
-		return null;
+		return films;
 	}
 
 	@Override
-	public void FilmfindById(int id) {
-		return;
+	public Film findById(int id) {
+	       for (Film film : films) {  
+	            if(film.getId() ==id){
+	                return film;
+	            }
+	        }
+	        return null;
 		
 	}
 
 	@Override
-	public void Filmsave(Film film) {
-		return;
+	public Film save(Film film) {
+		films.add(film);
+		return film;
 		
 	}
 }
