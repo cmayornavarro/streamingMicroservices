397d3555918dd2122542ac52189d94077409df02
cmayornavarro <cmayornavarro@gmail.com>
2021-02-08 18:42:08 +0100

    testing mondodb in main class

--- micostreaming/src/main/java/com/streaming/micostreaming/MicostreamingApplication.java
+++ micostreaming/src/main/java/com/streaming/micostreaming/MicostreamingApplication.java
@@ -1,13 +1,55 @@
 package com.streaming.micostreaming;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class MicostreamingApplication {
+import com.streaming.micostreaming.dao.CustomerDAO;
+import com.streaming.micostreaming.model.CustomerMongo;
 
-	public static void main(String[] args) {
-		SpringApplication.run(MicostreamingApplication.class, args);
-	}
+/*
+  implements CommandLineRunner, the run method is automatically invoked when Spring Boot starts.
+ * */
+@SpringBootApplication
+public class MicostreamingApplication implements CommandLineRunner {
+    /* spring Data MongoDB dynamically creates a proxy and injects it there */
+    @Autowired
+    private CustomerDAO repository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicostreamingApplication.class, args);
+
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("deleteAll");
+        repository.deleteAll();
+
+        // save a couple of customers
+        repository.save(new CustomerMongo("Alice", "Smith"));
+        repository.save(new CustomerMongo("Bob", "Smith"));
+
+        // fetch all customers
+        System.out.println("Customers found with findAll():");
+        System.out.println("-------------------------------");
+        for (CustomerMongo customer : repository.findAll()) {
+            System.out.println(customer);
+        }
+        System.out.println();
+
+        // fetch an individual customer
+        System.out.println("Customer found with findByFirstName('Alice'):");
+        System.out.println("--------------------------------");
+        System.out.println(repository.findByFirstName("Alice"));
+
+        System.out.println("Customers found with findByLastName('Smith'):");
+        System.out.println("--------------------------------");
+        for (CustomerMongo customer : repository.findByLastName("Smith")) {
+            System.out.println(customer);
+        }
+
+    }
 
 }
